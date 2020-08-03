c32f860aa53e8b3cd0325c687f153f9a931a3e05
서민주 <minju1307@gmail.com>
2020-08-03 09:52:23 +0900

    Initial Commit

---
+++ P1_Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  P1_Swift
+//
+//  Created by 서민주 on 2020/08/03.
+//  Copyright © 2020 minjuseo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
