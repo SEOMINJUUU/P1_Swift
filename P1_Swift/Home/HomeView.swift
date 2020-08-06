6942d6a9d3cdfad15d4243bcb1f4d3d26fe220ae
서민주 <minju1307@gmail.com>
2020-08-06 18:58:47 +0900

    Refactor HomeView to updated design

--- P1_Swift/Home/HomeView.swift
+++ P1_Swift/Home/HomeView.swift
@@ -8,23 +8,16 @@
 
 import SwiftUI
 
-struct Item: Identifiable {
-    var id = UUID()
-    var name: String
-    var location: String
-    var benefits: String
-    var image: String
-}
-
 struct HomeView: View {
+    
     var location: String = "역삼동"
-    @State var items: Array<Item> = [
-        Item(name: "웰릭스 건조기", location: "상암동", benefits: "웰릭스 5% 할인 쿠폰", image: "pickitem1"),
-        Item(name: "다이슨 청소기", location: "상암동", benefits: "다이슨 5% 할인 쿠폰", image: "pickitem2"),
-        Item(name: "보스_스피커 세트", location: "상암동", benefits: "동일 제품 구매시 20,000원 할인", image: "pickitem3"),
-        Item(name: "웰릭스 건조기", location: "상암동", benefits: "웰릭스 5% 할인 쿠폰", image: "pickitem1"),
-        Item(name: "다이슨 청소기", location: "상암동", benefits: "다이슨 5% 할인 쿠폰", image: "pickitem2"),
-        Item(name: "보스_스피커 세트", location: "상암동", benefits: "동일 제품 구매시 20,000원 할인", image: "pickitem3")]
+    var page: Int = 0
+    
+    @State var items: [Item] = [
+        Item(image: "pickitem1", name: "웰릭스 건조기", owner: "랑랑", distance: "1분 거리", benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
+        Item(image: "pickitem2", name: "다이슨 청소기", owner: "랑랑", distance: "1분 거리", benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
+        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: "랑랑", distance: "1분 거리", benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"])
+    ]
     
     var body: some View {
         NavigationView {
@@ -33,61 +26,54 @@ struct HomeView: View {
                 VStack {
                     HStack(spacing: 15){
                         Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                            Image(systemName: "chevron.down")
-                                .font(.headline)
-                                .foregroundColor(Color.init(UIColor.darkGray))
+                            Image(systemName: "chevron.down.circle.fill")
+                                .font(.title)
+                                .foregroundColor(Color.init(UIColor.systemBlue))
                         }
                         Text(location)
-                        Image(systemName: "circle.fill")
-                            .foregroundColor(Color.blue)
-                            .font(.system(size: 7))
-                            .padding([.top, .leading], -10)
                         Spacer()
                         Image(systemName: "magnifyingglass")
                     }
                 }
                 .padding()
                 
-                VStack(alignment: .center, spacing: 10) {
-                    Text("우리집에서\n수익을 얻어요")
-                        .multilineTextAlignment(.center)
-                    Text("내가 가지고 있는 제품을 보여주면 다양한 리워드를 얻을 수 있어요")
-                        .font(.caption)
-                        .padding(10)
-                    
-                    Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                        Text("보돈러를 위한 혜택보기")
-                            .font(.footnote)
-                    }
-                    .frame(width: 160, height: 30)
-                    .background(Color.blue)
-                    .cornerRadius(20)
-                    
-                    
-                }.padding(.vertical, 30)
-                    .frame(maxWidth: .infinity)
-                    .background(Color.black)
-                    .foregroundColor(Color.white)
+                Image("Home_Banner")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.bottom, 30)
+                //                    .frame(maxWidth: .infinity) // TODO: size
                 
-                Spacer()
-                Text("보고싶어요")
-                Rectangle()
-                    .frame(height: 2)
-                    .foregroundColor(Color.blue)
-                    .padding(.bottom, 10)
                 
                 VStack(alignment: .leading) {
                     
-                    Text("오늘의 Bodon 픽!\n")
-                        .fontWeight(.bold)
+                    Text("오늘의 Bodon 픽!")
+                        
+                        .fontWeight(.semibold)
                         .padding(.horizontal, 20)
-                        .padding(.bottom, -20)
-                    
-                    List(items) { item in
-                        ItemRow(item: item)
+                        
+                    VStack{
+                        PickedItemRow(item: items[page * 3])
+                        Divider()
+                        PickedItemRow(item: items[page * 3 + 1])
+                        Divider()
+                        PickedItemRow(item: items[page * 3 + 2])
+                        
+//                        ForEach(0..<3) { idx in
+//                            ItemRow(item: self.items[idx])
+//                            if idx < 2 {
+//                                Divider()
+//                            }
+//                        }
                     }
+                    .padding(.vertical, 10)
                     
                 }
+                HStack {
+                    Image(systemName: "circle.fill")
+                    Image(systemName: "circle.fill").foregroundColor(Color.gray)
+                    Image(systemName: "circle.fill").foregroundColor(Color.gray)
+                }.font(.system(size: 6))
+                Spacer()
             }
             .navigationBarHidden(true)
             .navigationBarTitle(Text(location))
@@ -102,21 +88,51 @@ struct HomeView_Previews: PreviewProvider {
     }
 }
 
-struct ItemRow: View {
+struct Item: Identifiable {
+    var id = UUID()
+    var image: String
+    var name: String
+    
+    var owner: String
+    var distance: String
+    var benefits: String
+    
+    var tags: [String]
+}
+
+struct PickedItemRow: View {
     var item: Item
     
     var body: some View {
-        HStack(alignment: .top) {
+        HStack(alignment: .center) {
             Image(item.image)
+            
             VStack(alignment: .leading, spacing: 3) {
-                Text(item.name).font(.subheadline)
-                Text(item.location).font(.caption).foregroundColor(Color.gray)
-                Text(item.benefits).font(.caption).foregroundColor(Color.blue)
+                Text(item.name)
+                    .font(.subheadline)
+                    .bold()
+                    .foregroundColor(Color.init(UIColor.darkGray))
+                HStack(spacing: 7) {
+                    Text(item.owner)
+                        .bold()
+                        .foregroundColor(Color.orange)
+                    Text(item.distance)
+                        .bold()
+                    Text(item.benefits)
+                        .foregroundColor(Color.blue)
+                }
+                .font(Font.system(size: 12))
+                HStack {
+                    ForEach(item.tags, id: \.self) {tag in
+                        Text("#\(tag)").font(.caption).foregroundColor(Color.gray)
+                    }
+                    
+                }
             }.padding(10)
             
-            Spacer()
+            
             Image(systemName: "bookmark")
-                .padding(15)
+                .padding(.top, 10)
                 .foregroundColor(Color.gray)
         }
     }
