832b888ab9d0873657c79df83da0443b1a4fe788
서민주 <minju1307@gmail.com>
2020-08-20 21:16:42 +0900

    Refactor HomeView to show banner page number and modify PickedItemRow to be reused in other views

--- P1_Swift/Home/HomeView.swift
+++ P1_Swift/Home/HomeView.swift
@@ -11,18 +11,19 @@ import SwiftUI
 struct HomeView: View {
     
     var location: String = "역삼동"
-    var page: Int = 0
+    var bannerCurrent: Int = 1
+    var bannerTotal: Int = 5
     
     @State var items: [Item] = [
-        Item(image: "pickitem1", name: "웰릭스 건조기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem2", name: "다이슨 청소기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem1", name: "웰릭스 건조기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem2", name: "다이슨 청소기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem1", name: "웰릭스 건조기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem2", name: "다이슨 청소기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"]),
-        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"])
+        Item(image: "pickitem1", name: "웰릭스 건조기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem2", name: "다이슨 청소기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem1", name: "웰릭스 건조기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem2", name: "다이슨 청소기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem1", name: "웰릭스 건조기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem2", name: "다이슨 청소기", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false),
+        Item(image: "pickitem3", name: "보스 E5000 스피커 세트", owner: ["랑랑"], distance: ["1분 거리"], benefits: "3,000원 / 5만원 LG쿠폰", tags: ["소음들어보세요","실제세탁보여드림"], bookmarked: false)
     ]
     
     var body: some View {
@@ -33,23 +34,43 @@ struct HomeView: View {
                 Image("Home_Banner")
                     .resizable()
                     .scaledToFit()
-                    .padding(.bottom, 30)
-                //                    .frame(maxWidth: .infinity) // TODO: size
+                
+                HStack {
+                    Spacer()
+                    
+                    Text("\(bannerCurrent)/\(bannerTotal)")
+                        .font(.caption)
+                        .padding(.horizontal, 10)
+                        .padding(.vertical, 2)
+                        .background(Color.white.opacity(0.2))
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .foregroundColor(Color.white)
+                        .background(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(Color.white, lineWidth: 1)
+                        )
+                        .offset(y: -40)
+                        
+                }.padding(.horizontal, 15)
                 
                 
                 VStack(alignment: .leading) {
                     
                     Text("오늘의 Bodon 픽!")    // list section title
-                        
                         .fontWeight(.semibold)
                         .padding(.horizontal, 20)
                     
-                    VStack{
-                        NavigationLink(destination: DetailView()) {
-                            List(items) { item in
-                                ItemRow(item:item)
+                    VStack {
+                        
+                        List(items) { item in
+                            ZStack {
+                                PickedItemRow(item:item)
+                                NavigationLink(destination: DetailView()) {
+                                    EmptyView()
+                                }.buttonStyle(PlainButtonStyle())
                             }
-                        }.buttonStyle(PlainButtonStyle())
+                        }
+                        
                     }
                     .padding(.vertical, 10)
                     
@@ -75,37 +96,54 @@ struct Item: Identifiable {
     var name: String
     
     var owner: [String]?
-    var distance: [String]
-    var benefits: String
+    var distance: [String]?
+    var benefits: String?
     
     var tags: [String]?
     var host: Int?
     var guest: Int?
+    
+    var bookmarked: Bool?
 }
 
 struct PickedItemRow: View {
     var item: Item
+    
+    var hostInfo: some View {
+        if let owner = item.owner?[0], let distance = item.distance?[0], let benefits = item.benefits {
+            return AnyView(HStack(spacing: 7) {
+                Text(owner)
+                    .bold()
+                    .foregroundColor(Color.orange)
+                Text(distance)
+                    .bold()
+                Text(benefits)
+                    .foregroundColor(Color.blue)
+            }
+            .font(Font.system(size: 12)))
+        } else {
+            return AnyView(EmptyView())
+        }
+    }
     
     var body: some View {
         
         HStack(alignment: .center) {
             Image(item.image)
             
-            VStack(alignment: .leading, spacing: 3) {
-                Text(item.name)
-                    .font(.subheadline)
-                    .bold()
-                    .foregroundColor(Color.init(UIColor.darkGray))
-                HStack(spacing: 7) {
-                    Text(item.owner?[0] ?? "")
+            VStack(alignment: .leading, spacing: 5) {
+                HStack {
+                    Text(item.name)
+                        .font(.subheadline)
                         .bold()
-                        .foregroundColor(Color.orange)
-                    Text(item.distance[0])
-                        .bold()
-                    Text(item.benefits)
-                        .foregroundColor(Color.blue)
+                        .foregroundColor(Color.init(UIColor.darkGray))
+                    Spacer()
+                    Image(systemName: "bookmark")
+                        .padding(.top, 10)
+                        .foregroundColor(Color.gray)
                 }
-                .font(Font.system(size: 12))
+                hostInfo
+                
                 HStack {
                     ForEach(item.tags ?? [], id: \.self) {tag in
                         Text("#\(tag)").font(.caption).foregroundColor(Color.gray)
@@ -114,10 +152,6 @@ struct PickedItemRow: View {
                 }
             }.padding(10)
             
-            
-            Image(systemName: "bookmark")
-                .padding(.top, 10)
-                .foregroundColor(Color.gray)
         }
         
     }
@@ -134,7 +168,7 @@ struct HomeTitleBar: View {
                 Button(action: {}) {
                     Image(systemName: "chevron.down.circle.fill")
                         .font(.title)
-                        .foregroundColor(Color.init(UIColor.systemBlue))
+                        .foregroundColor(Color("mainColor"))
                 }
                 Text(location)
                 Spacer()
