7bac59697255dd66bba1f9c0dec5c934017e7b58
서민주 <minju1307@gmail.com>
2020-08-20 21:14:46 +0900

    Refactor DetailView to use subviews & Add host profile images

--- P1_Swift/Home/DetailView.swift
+++ P1_Swift/Home/DetailView.swift
@@ -9,26 +9,43 @@
 import SwiftUI
 
 struct DetailView: View {
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
-    var location: String = "역삼동"
-    var item: Item = Item(image: "pickitem1", name: "바디프렌드 팬텀2 코어" , distance: ["걸어서 5분", "걸어서 5분", "걸어서 15분"], benefits: "100,000원 할인 / LG쿠폰", host: 105, guest: 3515)
+    var btnBack : some View { Button(action: {
+        self.presentationMode.wrappedValue.dismiss()
+    }) {
+        Image(systemName: "chevron.left")
+            .foregroundColor(Color.black)
+        }
+    }
+    
+    var item: Item = Item(image: "bodyfriend4", name: "바디프렌드 팬텀2 코어" , distance: ["걸어서 5분", "걸어서 5분", "걸어서 15분"], benefits: "제품 구매 시 50,000원 할인", host: 105, guest: 3515)
     var hosts: [Host] = [
-        Host(name: "신나는 아보카도", location: "길현제2동", providing: 117, distance: "걸어서 5분", badge: ["red": "우리 동네 BEST"]),
-        Host(name: "오늘도 하하하", location: "길현제2동", providing: 117, distance: "걸어서 5분", badge: ["green": "위치 기반 추천"]),
-        Host(name: "내일도 아보카도", location: "길현제2동", providing: 117, distance: "걸어서 5분")
+        Host(image: "host1", name: "신나는 뚱이", location: "길현제2동", providing: 117, distance: "걸어서 5분", tags: ["직접사용해보세요", "언제든지방문가능"]),
+        Host(image: "host2", name: "오늘도 하하하", location: "길현제2동", providing: 117, distance: "걸어서 5분", tags: ["타제품과비교가능"]),
+        Host(image: "host3", name: "내일도 아보카도", location: "길현제2동", providing: 117, distance: "걸어서 15분", tags: ["프라이빗한공간에서체험가능"])
     ]
+    
+    var relativeItems: [Item] = [
+        Item(image: "bodyfriend1", name: "바디프렌드 엘리제", benefits: "바디프렌드 5% 할인 쿠폰"),
+        Item(image: "bodyfriend2", name: "바디프렌드 엘리제", benefits: "바디프렌드 5% 할인 쿠폰"),
+        Item(image: "bodyfriend3", name: "바디프렌드 엘리제", benefits: "바디프렌드 5% 할인 쿠폰"),
+        Item(image: "bodyfriend4", name: "바디프렌드 엘리제", benefits: "바디프렌드 5% 할인 쿠폰")]
     
     var page: Int = 0
     
     var body: some View {
         
         VStack {
+            HStack {
+                btnBack
+                Spacer()
+            }.padding(.horizontal)
             
-            HomeTitleBar(location: location)
-            Spacer()
+            
             ScrollView {
                 ItemDetailRow(item: item)
-                
+                Spacer()
                 MapView()
                     .frame(height: 200)
                 
@@ -63,30 +80,15 @@ struct DetailView: View {
                         .bold()
                         .padding()
                     
-                    ScrollView(.horizontal) {
-                        HStack {
-                            VStack(alignment: .leading, spacing: 10) {
-                                Image("listitem1")
-                                    .resizable()
-                                    .scaledToFit()
-                                Text("바디프렌드 엘리제")
-                                    .font(.subheadline)
-                                    .bold()
-                                    .foregroundColor(Color.gray)
-                                Text("바디프렌드 5% 할인 쿠폰")
-                                    .font(.subheadline)
-                                    .bold()
-                                    .foregroundColor(Color.blue)
-                            }
-                            .frame(maxWidth: 170, maxHeight: 170)
-                            .padding()
-                            
-                        }.padding()
-                    }
+                    RelativeItemsRow(relativeItems: relativeItems)
                 }
+                
             }
+            .navigationBarTitle(Text("상세페이지"))
+            .navigationBarHidden(true)
+            .navigationBarBackButtonHidden(true)
+            
         }
-        
     }
     
 }
@@ -120,13 +122,16 @@ struct ItemDetailRow: View {
     var body: some View {
         HStack(alignment: .top) {
             Image(item.image)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 90, height: 90)
             
             VStack(alignment: .leading, spacing: 3) {
                 Text(item.name)
                     .font(.headline)
                     .bold()
                 HStack(spacing: 7) {
-                    Text(item.benefits)
+                    Text(item.benefits ?? "")
                         .foregroundColor(Color.blue)
                 }
                 .font(.footnote)
@@ -138,25 +143,25 @@ struct ItemDetailRow: View {
                 .font(.footnote)
                 .foregroundColor(Color.gray)
                 
-                
-            }.padding(10)
-            
+            }.padding(15)
             Spacer()
             
             Image(systemName: "bookmark")
                 .foregroundColor(Color.gray)
+                .padding(10)
         }.padding()
     }
 }
 
 struct Host: Identifiable {
     var id = UUID()
+    var image: String
     var name: String
     var location: String
     var providing: Int
     var distance: String
     
-    var badge: [String: String]?
+    var tags: [String]?
 }
 
 struct HostRow: View {
@@ -168,43 +173,72 @@ struct HostRow: View {
             //            BadgeRoundRectangle(host.badge.0, host.badge.1)
             
             HStack(spacing: 20) {
-                Image("listitem1")
+                Spacer()
+                Image(host.image)
                     .resizable()
                     .clipShape(Circle())
                     .frame(width: 50, height: 50)
                     .scaledToFit()
-                VStack(alignment: .leading, spacing: 10) {
-                    Text("\(host.name)")
-                        .font(.subheadline)
-                        .bold()
-                    
-                    Text("\(host.location) / 체험제공 \(host.providing)회")
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    HStack {
+                        Text("\(host.name)")
+                            .font(.subheadline)
+                            .bold()
+                        Spacer()
+                        Text("\(host.distance)")
+                            .foregroundColor(Color.blue)
+                            .font(.subheadline)
+                            .bold()
+                    }
+                    Text("체험제공 \(host.providing)회")
                         .font(.caption)
-                        .foregroundColor(Color.gray)
+                        .foregroundColor(Color.blue)
+                    
+                    HStack {
+                        ForEach(host.tags ?? [], id: \.self) {tag in
+                            Text("#\(tag)").font(.caption).foregroundColor(Color.gray)
+                        }
+                    }
                 }
                 
                 Spacer()
                 
-                Text("\(host.distance)")
-                    .foregroundColor(Color.blue)
-                    .font(.subheadline)
-                    .bold()
             }
-        }.padding(.horizontal, 20)
+        }
+        .padding(.vertical, 10)
     }
 }
 
-//struct BadgeRoundRectangle: View {
-//    var badge: [String: String]
-////    var color: String
-////    var text: String
-//
-//    var body: some View {
-//        Text("   \(badge)   ")
-//            .font(.caption)
-//            .padding(5)
-//            .foregroundColor(Color.gray)
-//            .overlay(RoundedRectangle(cornerRadius: 40)
-//                .stroke(Color.gray, lineWidth: 0.3))
-//    }
-//}
+struct RelativeItemsRow: View {
+    var relativeItems: [Item]
+    
+    var body: some View {
+        ScrollView(.horizontal) {
+            HStack(spacing: -20) {
+                ForEach(relativeItems) { item in
+                    VStack(alignment: .leading, spacing: 10) {
+                        
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(10)
+                            .shadow(color: Color.init(UIColor.systemGray4), radius: 5)
+                            .frame(width: 140, height: 140)
+                        
+                        Text(item.name)
+                            .font(.footnote)
+                            .bold()
+                            .foregroundColor(Color.gray)
+                        Text(item.benefits ?? "")
+                            .font(.footnote)
+                            .bold()
+                            .foregroundColor(Color.blue)
+                    }
+                    .frame(maxWidth: 170, maxHeight: 170)
+                    .padding()
+                }
+            }
+        }
+    }
+}
